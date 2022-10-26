b0ffe406092e351cbce9f5f310df8612d2213b9d
EDyugaeva <ed2408@yandex.ru>
2022-10-26 21:13:18 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
-    public void swap(int first, int second) {
+    public static void swap(int first, int second) {
+        first = second ^ first;
+        second = first ^ second;
+        first = first ^ second;
+
+        System.out.println(first);
+        System.out.println(second);
     }
+
+
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,44 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (month <= 0 || month > 12) {
+            System.out.println("invalid date");
+
+        }
+        boolean leap;
+        if (year % 100 == 0 && year % 400 != 0) {
+            leap = false;
+        } else if (year % 4 == 0) {
+            leap = true;
+        }
+        else {
+            leap = false;
+        }
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println(31);
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println(30);
+                break;
+            case 2:
+                if (leap) {
+                    System.out.println("29");
+                    break;
+                }
+                else {
+                    System.out.println("28");
+                }
+        }
 
     }
 }
